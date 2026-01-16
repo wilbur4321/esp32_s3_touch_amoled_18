e86a625aa684cc1210bf2156d4167707a406f851
Randy Chapman <wilbur4321@gmail.com>
2026-01-15 21:01:57 -0800

    working start

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,21 @@
 #include <Arduino.h>
+#include "HWCDC.h"
+
+HWCDC USBSerial;
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
+  USBSerial.begin(115200);
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  USBSerial.println("Hello World");
+  delay(1000);
 }
 
 // put function definitions here:
